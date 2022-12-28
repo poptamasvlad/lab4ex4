be31070481596f97870792c5abd6d63804fb5869
Poptamas Vlad <poptamasvlad@gmail.com>
2022-12-28 15:25:52 +0200

    Add project files.

---
+++ lab4ex4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace lab4ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Scrieti o functie recursiva care va afisa in ordine elementele unui vector de
+            intregi. */
+
+            int n = int.Parse(Console.ReadLine());
+            int[] vector = new int[n];
+
+            for(int i = 0; i < n; i++)
+            {
+                vector[i] = int.Parse(Console.ReadLine());
+            }
+
+            Vector(n, vector);
+        }
+        static void Vector(int n, int[] vector)
+        {
+            if(n > 1)
+            {
+                Vector(n-1 , vector);
+            }
+            Console.Write( vector[n - 1] + " " );
+        }
+    }
+}
